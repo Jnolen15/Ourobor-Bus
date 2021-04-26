113265b85e21e5d1b0f4da468ee9649e5a04ebfc
Nathann Latimore <nlatimor@ucsc.edu>
2021-04-25 18:50:53 -0700

    Added 4 more patterns and adjusted previous ones

--- src/prefabs/Spawner.js
+++ src/prefabs/Spawner.js
@@ -7,7 +7,9 @@ class Spawner {
         this.busRight = busRight;
 
         // | how high objects spawn on the screen
-        this.yPos = 0;
+        this.yPos = -100;
+        // | how spaced out objects are when they're stacked in a column
+        this.stackDistance = 80;
         // | placement of spawners by x position (pixels);
         this.xPos1 = 80;
         this.xPos2 = 160;
@@ -15,10 +17,15 @@ class Spawner {
         this.xPos4 = 320;
         this.xPos5 = 400;
         // | list of all possible spawn patterns
-        let pat1 = { c1:'none', c2:'obst', c3:'none', c4:'obst', c5:'ped3'}
-        let pat2 = { c1:'none', c2:'obst', c3:'ped3', c4:'obst', c5:'ped3'}
-        let pat3 = { c1:'obst', c2:'ped1', c3:'none', c4:'ped1', c5:'obst'}
-        this.patternList = [pat1, pat2, pat3];
+        let pat0 = { c1:'none', c2:'obst', c3:'none', c4:'obst', c5:'ped3'}
+        let pat1 = { c1:'none', c2:'obst', c3:'ped3', c4:'obst', c5:'ped3'}
+        let pat2 = { c1:'obst', c2:'mPed', c3:'none', c4:'mPed', c5:'obst'}
+        let pat3 = { c1:'none', c2:'none', c3:'sTop', c4:'sMid', c5:'sBot'} // Triangle shape (sloping left)
+        let pat4 = { c1:'sBot', c2:'sMid', c3:'sTop', c4:'none', c5:'none'} // Triangle shape (sloping right)
+        let pat5 = { c1:'sTop', c2:'none', c3:'obst', c4:'none', c5:'sBot'}
+        let pat6 = { c1:'sBot', c2:'none', c3:'obst', c4:'none', c5:'sTop'}
+        let pat7 = { c1:'obst', c2:'mPed', c3:'hPed', c4:'mPed', c5:'obst'}
+        this.patternList = [pat0, pat1, pat2, pat3, pat4, pat5, pat6, pat7];
         // | spawn timer setup
         let timerConfig = {
             delay: 3000, // milliseconds
@@ -52,29 +59,60 @@ class Spawner {
     }
 
     columnSpawn(key, xPos) {
+        let currObst;   // current obstacle
+        let currPed;    // current pedestrian
         switch(key) {
             case 'none':
                 // | do nothing
                 break;
             case 'obst':
                 // | spawn obstacle
-                let obstacle = new Obstacle(this.scene, xPos, 0, 'testObstacle');
-                this.setObsCollision(this.bus, obstacle);
+                currObst = new Obstacle(this.scene, xPos, this.yPos, 'testObstacle');
+                this.setObsCollision(this.bus, currObst);
                 break;
-            case 'ped1':
-                // | spawn a single pedestrian
-                let pedestrian = new Pedestrian(this.scene, xPos, 0, 'testObstacle1', 1);
-                this.setPedCollision(this.bus, this.busLeft, this.busRight, pedestrian);
+            case 'mPed':
+                // | spawn a single pedestrian in the middle
+                currPed = new Pedestrian(this.scene, xPos, this.yPos, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
+                break;
+            case 'hPed':
+                // | spawn a single pedestrian slightly higher
+                currPed = new Pedestrian(this.scene, xPos, this.yPos-this.stackDistance, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
                 break;
             case 'ped3':
-                // | spawn a column of 3 pedestrians
-                let ped1 = new Pedestrian(this.scene, xPos, this.yPos, 'testObstacle1', 1);
-                let ped2 = new Pedestrian(this.scene, xPos, this.yPos-80, 'testObstacle1', 1);
-                let ped3 = new Pedestrian(this.scene, xPos, this.yPos-160, 'testObstacle1', 5);
-                this.setPedCollision(this.bus, this.busLeft, this.busRight, ped1);
-                this.setPedCollision(this.bus, this.busLeft, this.busRight, ped2);
-                this.setPedCollision(this.bus, this.busLeft, this.busRight, ped3);
+                // --- Spawn a column of 3 pedestrians
+                // | pedestrian 1
+                currPed = new Pedestrian(this.scene, xPos, this.yPos+this.stackDistance, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
+                // | pedestrian 2
+                currPed = new Pedestrian(this.scene, xPos, this.yPos, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
+                // | pedestrian 3
+                currPed = new Pedestrian(this.scene, xPos, this.yPos-this.stackDistance, 'testObstacle1', 5);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
                 break;
+            case 'sBot':
+                // | spawn a obstacle with a ped in front (lower on the screen) 
+                currObst = new Obstacle(this.scene, xPos, this.yPos, 'testObstacle');
+                this.setObsCollision(this.bus, currObst);
+                currPed = new Pedestrian(this.scene, xPos, this.yPos+this.stackDistance, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
+                break;
+            case 'sMid':
+                currObst = new Obstacle(this.scene, xPos, this.yPos-this.stackDistance, 'testObstacle');
+                this.setObsCollision(this.bus, currObst);
+                currPed = new Pedestrian(this.scene, xPos, this.yPos, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
+                break;
+            case 'sTop':
+                currObst = new Obstacle(this.scene, xPos, this.yPos-(this.stackDistance*2), 'testObstacle');
+                this.setObsCollision(this.bus, currObst);
+                currPed = new Pedestrian(this.scene, xPos, this.yPos-this.stackDistance, 'testObstacle1', 1);
+                this.setPedCollision(this.bus, this.busLeft, this.busRight, currPed);
+                break;
+            default:
+                console.log("ERROR - Unknown column pattern key: " + key);
         }
     }
 
