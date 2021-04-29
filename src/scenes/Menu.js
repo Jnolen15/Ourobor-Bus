294be5d8906fe2bcdaa56b026c0074f10c7ce122
benurlik <baurlik@ucsc.edu>
2021-04-28 18:31:51 -0700

    added preloading to the main menu and fixed bug

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,6 +3,43 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    preload() {
+        // Add sprites
+        this.load.image('blood', './assets/blood.png');
+        this.load.image('money', './assets/money.png');
+        this.load.image('bus', './assets/BUSsprite.png');
+        // Street background
+        this.load.image('street', './assets/street_background_5lane.png');
+        this.load.image('hellStreet', './assets/street_background_hell.png');
+        this.load.image('heavenStreet', './assets/street_background_heaven.png');
+        // Transitions
+        this.load.image('hellTransition', './assets/transition_Hell.png');
+        this.load.image('heavenTransition', './assets/transition_Heaven.png');
+        // Earth Obstacles
+        this.load.image('car1', './assets/carsprite_1.png');
+        this.load.image('car2', './assets/carsprite_2.png');
+        this.load.image('car3', './assets/carsprite_3.png');
+        this.load.image('tree', './assets/obstacle_tree.png');
+        // Earth Pedestrians
+        this.load.image('ped1', './assets/pedestrian_1.png');
+        this.load.image('ped2', './assets/pedestrian_2.png');
+        this.load.image('ped3', './assets/pedestrian_3.png');
+        this.load.image('rped1', './assets/richpedestrian_1.png');
+        // Extra
+        this.load.image('testObstacle', './assets/testObstacle.png');
+        // Add Auido
+        this.load.audio('music', './assets/Ourobor-Bus_Earth.wav');
+        this.load.audio('hellMusic', './assets/Ourobor-Bus_Hell.wav');
+        this.load.audio('heavenMusic', './assets/Ourobor-Bus_Heaven.wav');
+        this.load.audio('oHit', './assets/ObstacleHit.wav');
+        this.load.audio('pHit', './assets/passengerHit.wav');
+        this.load.audio('pHit2', './assets/passengerHit2.wav');
+        this.load.audio('pHit3', './assets/passengerHit3.wav');
+        this.load.audio('pPickup', './assets/passengerPickup.wav');
+        this.load.audio('pPickup2', './assets/passengerPickup2.wav');
+        this.load.audio('pPickup3', './assets/passengerPickup3.wav');
+    }
+
     create() {
         // menu text configuration
         let menuConfig = {
