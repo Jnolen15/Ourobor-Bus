85a9923dfc9b4c5a1e79ea449ed33cc5e785d04b
Nathann Latimore <nlatimor@ucsc.edu>
2021-04-29 14:15:21 -0700

    Fixed destroyAll function and foreach loops

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -138,18 +138,16 @@ class Play extends Phaser.Scene {
             this.bus.update();
         
         // update objects (pedestrians and obstacles)
-        if (this.objects.length >= 1) {
-            this.objects.forEach(
-                (item, index)=>{
-                    if (item.active == true) {
-                        // update the item if it still exists 
-                        item.update();
-                    }
-                    else {
-                        // remove the item if it is no longer active
-                        this.objects.splice(index, 1);
-                    }
-                });
+        for (let i = 0; i < this.objects.length; i++) {
+            if (this.objects[i].active == true) {
+                // update the item if it still exists 
+                this.objects[i].update();
+            }
+            else {
+                // remove the item if it's no longer active
+                this.objects.splice(i, 1);
+                i--;
+            }
         }
 
         // Update score
@@ -348,11 +346,14 @@ class Play extends Phaser.Scene {
             delay: 2000, // milliseconds
             callback: () => {
                 // make change
+                console.log("This many before: " + this.objects.length);
+                console.log("SWAPPING");
                 this.street.alpha = 0;
                 this.hellStreet.alpha = 0;
                 this.heavenStreet.alpha = 1;
                 this.destroyAll();
                 this.prevLocation = 'Heaven';
+                console.log("This many after: " + this.objects.length);
             },
             callbackScope: this,
         }
@@ -360,19 +361,16 @@ class Play extends Phaser.Scene {
     }
 
     destroyAll(){
-        if (this.objects.length >= 1) {
-            this.objects.forEach(
-                (item, index)=>{
-                    if (item.active == true) {
-                        // update the item if it still exists 
-                        item.destroy();
-                        this.objects.splice(index, 1);
-                    }
-                    else {
-                        // remove the item if it is no longer active
-                        this.objects.splice(index, 1);
-                    }
-                });
+        while (this.objects.length != 0) {
+            if (this.objects[0].active == true) {
+                // destroy the item if it still exists 
+                this.objects[0].destroy();
+                this.objects.splice(0, 1);
+            }
+            else {
+                // remove the item if it's no longer active
+                this.objects.splice(0, 1);
+            }
         }
     }
 
