48297e31be797434196ad26753aa812faa9a40b6
Nathann Latimore <nlatimor@ucsc.edu>
2021-05-01 19:50:37 -0700

    Updated tutorial slides with new images

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,9 +7,9 @@ class Menu extends Phaser.Scene {
         // Menu images
         this.load.image('menuScreen', './assets/menuscreen.png');
         // tutorial images
-        this.load.image('tutor1', './assets/tutorialp1.png');
-        this.load.image('tutor2', './assets/tutorialp2.png');
-        this.load.image('tutor3', './assets/tutorialp3.png');
+        this.load.image('tutor1', './assets/tutorial1.png');
+        this.load.image('tutor2', './assets/tutorial2.png');
+        this.load.image('tutor3', './assets/tutorial3.png');
         // Add sprites
         this.load.image('UI', './assets/uiBack.png');
         this.load.image('end', './assets/uiEnd.png');
